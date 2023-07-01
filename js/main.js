34eee5f8673440471775f5b1e6166a943ee1facc
Oki John <blackpopex@yahoo.com>
2023-07-01 01:25:07 +0100

    completed the code for payment gateway and also connected it to paystack, it is working 100% perfectly

--- js/main.js
+++ js/main.js
@@ -95,29 +95,57 @@ function topFunction() {
 //CALL THIS FUNCTION TO CHECK IF AN DONATION AMOUNT HAS BEEN PICKED, IF TRUE, GET THE NEXT BUTTON AND ADD THE CLASS "NEXT", TO ENABLE JQUERY ASSESSS THE CLASS TO MOVE THE USER TO THE NEXT PAGE CONTAINING PAYMENT DETAILS.
 var preBtn = document.getElementById("Prev");
 var nextBtn = document.getElementById("Next");
+var amountSelected; 
+var myInputedAmount = document.getElementById("myInputAmount").value = "";
 
+function myFunction(val) {
+  amountSelected = val; 
+}
+
+//RUN THIS FUNCTION ONLY IF AN AMOUNT HAS BEEN PICKED
 function sayHello() {
   var clickedEvent = document.querySelectorAll(".amount");
   var Result = document.getElementById("showResult");
   Result.innerHTML = "Select an Amount";
-
+   
+//RUN THIS CODE ONLY IF THE DONATION AMOUNT CONTAINS THE CLASSLIST CLICKED, THE CODE IS USED TO CONVERT NEXT BUTTON TO SUBMIT BTN AS WELL. 
   for (let v = 0; v < clickedEvent.length; v++) {
     let m = "";
     m += clickedEvent[v].className; 
     if (m.includes("clicked")) {
+      /**
           console.log(m) + "<br/>";
+           */
       nextBtn.classList.add("Next");
-      Result.innerHTML = ""; 
-        if (nextBtn.classList.contains("Next")) {
-          setTimeout(function () {
-            nextBtn.type = "submit";
-          }, 3000);
-        } else {
-          nextBtn.type = "button";
+      Result.innerHTML = "";
+      if (nextBtn.classList.contains("Next")) {
+        setTimeout(function () {
+          nextBtn.type = "submit";
+        }, 3000);
+      } else {
+        nextBtn.type = "button";
       }
     }
   }
 }
+
+//THIS CONTROLS THE INPUT FIELDS FOR THE DONATION FORM. 
+//Get all input field and set the values to empty
+   var firstName = document.getElementById("firstName").value = "";
+   var lastName = document.getElementById("lastName").value = "";
+   var email = document.getElementById("e_mail").value = "";
+   var PhoneNumber = document.getElementById("PhoneNumber").value = "";
+//Use this to check if input filed is empty, if empty set submit BTN to false else set to true
+if (nextBtn.type === "submit") {
+  $(document).ready(function () {
+    $("#Next").attr("disabled", true);
+    $("#firstName").keyup(function () {
+      if ($(this).val().length != 0) $("#Next").attr("disabled", false);
+      else $("#Next").attr("disabled", true);
+    });
+  });
+}
+
 //Add Event LISTENER TO THIS BTN TO CHANGE THE BUTTON TYPE BACK TO BUTTON IF PREVIOUS BTN IS PRESSED
 preBtn.addEventListener("click", function () {
     nextBtn.type = "button";
@@ -141,8 +169,10 @@ for (w = 0; w < donationAmount.length; w++){
     amountSelected = "";
     if (this.classList.contains("clicked")) {
       amountSelected = this.value;
+      /**
       console.log(amountSelected);
       console.log(this.className);
+       */
       //IF CLASSLIST CONTAINS CLICKED, ADD A CLASS TO THE NEXT BUTTON
       document.getElementById("Next").classList.add("Next");
       //THEN ACTIVATE THIS JQUERY CODE
@@ -203,13 +233,108 @@ for (n = 0; n < Frequency.length; n++) {
     }
     this.classList.add("clicked");
     frequencyValue = this.value;
+    /*
     console.log(frequencyValue);
+    */
   })
 }
+
+form.addEventListener("submit", payWithPaystack, false);
+  
+  function payWithPaystack(e) {
+  e.preventDefault();
+//Get form data
+   const data = new FormData(form);
+    for (const [name, value] of data) {
+    console.log(name + ":" + value);
+    console.log("Frequency:" + frequencyValue);
+    console.log("Donation Amount:" + amountSelected);
+    if (value != "") {
+
+    let handler = PaystackPop.setup({
+      key: "pk_test_995cbe8b097a32ca0dc3a7801ca21b78eb6401fc", // Replace with your public key
+
+      email: document.getElementById("e_mail").value,
+      amount: amountSelected * 100,
+
+      ref: "" + Math.floor(Math.random() * 1000000000 + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+
+      // label: "Optional string that replaces customer email"
+
+      onClose: function () {
+        alert("Window closed.");
+      },
+
+      callback: function (response) {
+        let message = "Payment complete! Reference: " + response.reference;
+
+        alert(message);
+      },
+    });
+
+    handler.openIframe();
+      }
+    }
+  }
+
 
 
 /***********************CONTROLS THE PAYMENT GATEWAY END***************************/
 
+/****
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const data = new FormData(form);
+  for (const [name, value] of data) {
+    console.log(name + ":" + value);
+    console.log("Frequency:" + frequencyValue);
+    console.log("Donation Amount:" + amountSelected);
+    if (value != "") {
+      //Call paystack function here
+
+      //Paystack Integration
+      const paymentForm = document.getElementById("form");
+
+      paymentForm.addEventListener("submit", payWithPaystack, false);
+
+      function payWithPaystack(e) {
+        e.preventDefault();
+
+        let handler = PaystackPop.setup({
+          key: "pk_test_995cbe8b097a32ca0dc3a7801ca21b78eb6401fc", // Replace with your public key
+
+          email: document.getElementById("e_mail").value,
+
+          amount: amountSelected * 100,
+
+          ref: "" + Math.floor(Math.random() * 1000000000 + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+
+          // label: "Optional string that replaces customer email"
+
+          onClose: function () {
+            alert("Window closed.");
+          },
+
+          callback: function (response) {
+            let message = "Payment complete! Reference: " + response.reference;
+
+            alert(message);
+          },
+        });
+
+        handler.openIframe();
+      }
+    }
+  }
+});
+ 
+
+
+
+
+
+
+**** */
 
 
 
